4d48819a7624b3a522f3f82fc5ed3752c4480966
Matheus Alexandre <alexandremhm@gmail.com>
2022-04-08 20:28:05 -0300

    test 2

--- teste.js
+++ teste.js
@@ -1,37 +1,53 @@
 const mapValues = {
-    '1': 60,
-    '2': 75,
-    '3': 90,
-    '4': 85,
-    '5': 80
-}
-
-
-const mapedValues = {
-    "masculino": [
-        {'1': [0, 15]},
-        {'2': [16, 18]},
-        {'3': [19, 30]},
-        {'4': [31, 40]},
-        {'5': [41]},
+    masculino: {
+      1: 60,
+      2: 75,
+      3: 90,
+      4: 85,
+      5: 80,
+    },
+    feminino: {
+      1: 60,
+      2: 60,
+      3: 90,
+      4: 85,
+      5: 80,
+    },
+  };
+  
+  const mapedAges = {
+    masculino: [
+      { 1: [0, 15] },
+      { 2: [16, 18] },
+      { 3: [19, 30] },
+      { 4: [31, 40] },
+      { 5: [41, 0] },
     ],
-    "feminino": [
-        {'preço 1': [0, 18]},
-        {'preco 3': [19, 25]},
-        {'preco 4': [26, 40]},
-        {'preco 5': [41]},
+    feminino: [
+      { 1: [0, 15] },
+      { 2: [16, 18] },
+      { 3: [19, 30] },
+      { 4: [31, 40] },
+      { 5: [41, 0] },
     ],
-}
-
-const definePrice = (gender, age) => {
-    const clienteGenderPricesArr = mapedValues[gender]
-
-    for ( index = 0; index <= clienteGenderPricesArr.length; index ++) {
-       if ( age >= clienteGenderPricesArr[index][index + 1][0] || age <= clienteGenderPricesArr[index][index + 1][1]) {
-           return mapValues[index + 1]
-       }
+  };
+  
+  const definePrice = (gender, age) => {
+    const clienteGenderPricesArr = mapedAges[gender];
+  
+    for (index = 0; index <= clienteGenderPricesArr.length; index++) {
+      if (
+        age >= clienteGenderPricesArr[index][index + 1][0] &&
+        age <= clienteGenderPricesArr[index][index + 1][1]
+      ) {
+        return mapValues[gender][index + 1];
+      } else if (
+        age >= clienteGenderPricesArr[index][index + 1][0] &&
+        clienteGenderPricesArr[index][index + 1][1] === 0
+      ) {
+        return mapValues['5'];
+      }
     }
-
-}
-
-console.log(definePrice("masculino", 18))
+  };
+  
+  console.log(definePrice('feminino', 26));
